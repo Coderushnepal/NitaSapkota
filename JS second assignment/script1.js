9e2bc50c949b21231dcd2239d6465dc4b0453624
Neeta135 <sapkotaneeta30@gmail.com>
2020-07-17 14:31:55 +0545

    complete second assignment of javascript

--- JS second assignment/script1.js
+++ JS second assignment/script1.js
@@ -34,6 +34,72 @@ function Counts(array, letter) {
 console.log(Counts(lists, 's'));
 console.log(Counts(lists, 'a'));
 
+// Convert the array of the result in Question 1, in following format
+//3
+var res = function(array) {
+	var getArrayFromQ1 = result(array);
+	var organized = getArrayFromQ1.reduce(function(acc, value, index) {
+		delete value.id;
+		acc[index + 1] = value;
+		return acc;
+	}, {});
+	return organized;
+};
+
+console.log(res(lists));
+console.log('\n\n')
+
+
+//Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+function longestZero(str) {
+	return str.split('1').sort().pop().length;
+  }
+  
+  console.log(longestZero("01100001011000"));
+  console.log(longestZero("100100100"));
+  console.log(longestZero("11111"));
+  console.log(longestZero(""));
+  console.log('\n\n')
+
+  //Create a function that takes a string and replaces the vowels with another character.
+//a = 1, e = 2, i = 3, o = 4, u = 5
+
+function replaceVowel(str) {
+	var strArr = str.split('');
+	for (var i = 0; i < str.length; i++) {
+	  var char = str[i].toLowerCase();
+	  switch(char) {
+		case "a":
+			strArr[i] = 1;
+		  break;
+		case "e":
+			strArr[i] = 2;
+		  break;
+		case "i":
+			strArr[i] = 3;
+		  break;
+		case "o":
+			strArr[i] = 4;
+		  break;
+		case "u":
+			strArr[i] = 5;
+		  break;
+		default:
+			strArr[i];                                     
+	  }                                                    
+	                                                       
+	}
+	return strArr.join('');                                // .join() bina array ma return garchha ["K", "R", 3, "T", 3, "K", 1]            
+  }		                                                   // .join() pachi chai comma ma return garchha K,R,3,T,3,K,1 // .join('') pachi comma hatayera return garchha KR3T3K1
+                                                    
+console.log(replaceVowel("NEETA"));
+console.log(replaceVowel("imaska"));
+console.log(replaceVowel("sapkota"));
+console.log(replaceVowel("manali"));
+ 
+
+
+
 
  
 

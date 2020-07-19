5fe21a094099c9f46a3038f4fcbc2f2cde6c3ffa
Neeta135 <sapkotaneeta30@gmail.com>
2020-07-19 22:27:05 +0545

    Assignment 4 completed

---
+++ JS assignment 4/script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn');
+const box = document.getElementById('box');
+const height = box.clientHeight;
+const width = box.clientWidth;
+btn.onclick = function(e) {
+        var newCircle = document.createElement('div');
+        newCircle.classList.add('circle');
+        newCircle.style.position = "absolute";
+        newCircle.style.top = Math.random() * (height - 20) + 'px';
+        newCircle.style.left = Math.random() * (width - 20) + 'px';
+        box.appendChild(newCircle);
+        var direction = 1;
+        newCircle.addEventListener("click" , function() {
+          var  interval = setInterval(function(){
+                var nextTop = parseInt(newCircle.style.top) + 10 * direction;
+                newCircle.style.top = nextTop + "px";
+                if( nextTop >= (height - 20) || nextTop<= 0 ) {
+                   direction *= -1;
+                }
+            }, 1000/60)
+        });
+    };
